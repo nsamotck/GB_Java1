c636c6bfc0218d24fd1230aa4a401d1407f30f15
nsamotck <n.samotskiy@gmail.com>
2020-06-04 10:27:23 +0200

    Comment left in method fillArray after homework-check

--- src/main/java/ru/gb/lesson2/Lesson2.java
+++ src/main/java/ru/gb/lesson2/Lesson2.java
@@ -74,7 +74,7 @@ public class Lesson2 {
 
     /*2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;*/
     private static int[] fillArray(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) { //можно задать i = 0 и в цикле будет arr[i] = i * 3
             arr[i] = arr[i - 1] + 3;
         }
         return arr;
